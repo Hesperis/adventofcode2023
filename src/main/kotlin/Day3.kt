2bfd7885ebf5d67c4d971582be4b86f774ba552f
Karin Obermüller <karin.obermuller@recordedfuture.com>
2023-12-04 07:58:08 +0100

    refactor day 3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -12,53 +12,54 @@ class Day3 {
         val gears = findAllGears(input, numbers)
         return gears.sumOf { it.gearPair.first.value * it.gearPair.second.value }
     }
-}
 
-fun findAllNumbers(input: List<String>): List<Number> {
-    val numbers = mutableListOf<Number>()
-    input.mapIndexed { row, s ->
-        var currentNumber = ""
-        s.mapIndexed { index, c ->
-            currentNumber = when {
-                c.isDigit() -> currentNumber.plus(c)
-                else -> {
-                    if (currentNumber != "") numbers.add(Number(row, index - currentNumber.length, index, currentNumber.toInt()))
-                    ""
+    fun findAllNumbers(input: List<String>): List<Number> {
+        val numbers = mutableListOf<Number>()
+        input.mapIndexed { row, s ->
+            var currentNumber = ""
+            s.mapIndexed { index, c ->
+                currentNumber = when {
+                    c.isDigit() -> currentNumber.plus(c)
+                    else -> {
+                        if (currentNumber != "") numbers.add(Number(row, index - currentNumber.length, index, currentNumber.toInt()))
+                        ""
+                    }
                 }
             }
+            if (currentNumber != "") numbers.add(Number(row, s.length - 1 - currentNumber.length, s.length-1, currentNumber.toInt()))
         }
-        if (currentNumber != "") numbers.add(Number(row, s.length - 1 - currentNumber.length, s.length-1, currentNumber.toInt()))
+        return numbers
     }
-    return numbers
+
+    fun findAllSymbols(input: List<String>): List<Symbol> {
+        return input.mapIndexed { row, s ->
+            s.mapIndexed { index, c ->
+                if (c != '.' && !c.isDigit()) Symbol(row, index) else null
+            }.filterNotNull()
+        }.flatten()
+    }
+
+    fun findAllGears(input: List<String>, numbers: List<Number>): List<Gear> {
+        return input.mapIndexed { row, s ->
+            s.mapIndexed { index, c ->
+                if (c != '.' && !c.isDigit() && c == '*') {
+                    val attachedNumbers = numbers.filter {
+                        it.row in row - 1..row + 1 &&
+                                index in it.startIndex - 1..it.endIndex
+                    }
+                    if (attachedNumbers.size == 2) Gear(Symbol(row, index), Pair(attachedNumbers[0], attachedNumbers[1])) else null
+                } else null
+            }.filterNotNull()
+        }.flatten()
+    }
+    fun Number.hasAdjacentSymbol(symbols: List<Symbol>): Boolean {
+        return symbols.any {
+            it.row in this.row - 1..this.row + 1 &&
+                    it.index in this.startIndex - 1..this.endIndex
+        }
+    }
 }
 
-fun findAllSymbols(input: List<String>): List<Symbol> {
-    return input.mapIndexed { row, s ->
-        s.mapIndexed { index, c ->
-            if (c != '.' && !c.isDigit()) Symbol(row, index) else null
-        }.filterNotNull()
-    }.flatten()
-}
-
-fun findAllGears(input: List<String>, numbers: List<Number>): List<Gear> {
-    return input.mapIndexed { row, s ->
-        s.mapIndexed { index, c ->
-            if (c != '.' && !c.isDigit() && c == '*') {
-                val attachedNumbers = numbers.filter {
-                    it.row in row - 1..row + 1 &&
-                            index in it.startIndex - 1..it.endIndex
-                }
-                if (attachedNumbers.size == 2) Gear(Symbol(row, index), Pair(attachedNumbers[0], attachedNumbers[1])) else null
-            } else null
-        }.filterNotNull()
-    }.flatten()
-}
-fun Number.hasAdjacentSymbol(symbols: List<Symbol>): Boolean {
-    return symbols.any {
-        it.row in this.row - 1..this.row + 1 &&
-                it.index in this.startIndex - 1..this.endIndex
-    }
-}
 
 
 data class Number (val row: Int, val startIndex: Int, val endIndex: Int, val value: Int)
